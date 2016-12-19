709f0c8fa8b90b3cac3b46880296570af65b5781
Monali Patil <monali24u@gmail.com>
2016-12-18 19:02:39 -0700

    added more functions which checks conditional statements on given input string. Excercise Completed.

--- week2/function-practice-1/function.js
+++ week2/function-practice-1/function.js
@@ -16,8 +16,7 @@ function lastLetter(str){
 }
 
 var output = lastLetter("hello");
-console.log("lastLetter(hello) is:-   "+output);
-
+console.log("\nlastLetter(hello) is:-   "+output);
 var output = lastLetter("island");
 console.log("lastLetter(island) is:-  "+output);
 
@@ -28,7 +27,7 @@ function square(number){
 }
 
 var result = square(3);
-console.log("square(3) is:- "+ result);
+console.log("\nsquare(3) is:- "+ result);
 var result = square(5);
 console.log("square(5) is:- "+ result);
 
@@ -39,8 +38,97 @@ function negate(number){
 }
 
 var result = negate(5);
-console.log("negate(3) is:- "+ result);
+console.log("\nnegate(3) is:- "+ result);
 var result = negate(-8);
 console.log("negate(5) is:- "+ result);
 
 // -------------------------------------------
+
+function toArray(one, two, three){
+  var Array = [one, two, three];
+  return Array;
+}
+
+var result = toArray(1,4,5);
+console.log("\ntoArray(1,4,5) is:-  ["+ result +"]");
+var result = toArray(8,9,10);
+console.log("toArray(8,9,10) is:-  ["+ result +"]");
+
+// ---------------------------------------------
+
+function startsWithA(str){
+
+  if(str[0] === 'a' || str[0] === 'A'){
+    return true;
+  }
+  else{
+    return false;
+  }
+}
+
+var result = startsWithA("aardvark");
+console.log("\nstartsWithA('aardvark') is:- "+result);
+var result = startsWithA("bear");
+console.log("startsWithA('bear') is:- "+result);
+
+// ------------------------------------------------
+
+function excite(str){
+  return str+"!!!";
+}
+
+var result = excite("yes");
+console.log("\nexcite('yes') is:- "+result);
+var result = excite("go");
+console.log("excite('go') is:- "+result);
+
+// -----------------------------------------
+
+function sun(str){
+  var result = str.indexOf("sun");
+
+  if(result !== -1){
+    return true;
+  }
+  else if(result === -1){
+    return false;
+  }
+}
+
+var output = sun('sundries');
+console.log("\nsun('sundries') is:- " + output);
+var output = sun('asunder');
+console.log("sun('asunder') is:- " + output);
+var output = sun('catapult');
+console.log("sun('catapault') is:- " + output);
+
+// ----------------------------------------------
+
+function tiny(number){
+
+  if(number >= 0 && number <=1){
+    return true;
+  }
+  else{
+    return false;
+  }
+}
+
+var result = tiny(0.3);
+console.log("\ntiny(0.3) is:- "+result);
+var result = tiny(14);
+console.log("tiny(14) is:- "+result);
+var result = tiny(-5);
+console.log("tiny(-5) is:- "+result);
+// ----------------------------------------------
+
+function getSeconds(str){
+  var splited = str.split(':');
+  var result = +(splited[0] * 60) + +splited[1];
+  return result;
+}
+
+var outcome = getSeconds('01:30');
+console.log("\ngetSeconds('01:30') is:- "+ outcome);
+var outcome = getSeconds('10:25');
+console.log("getSeconds('10:25') is:- "+ outcome);
