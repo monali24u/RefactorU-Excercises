901ac1a10bcef710cc4774bb9b3c84c39c8cf852
Monali Patil <monali24u@gmail.com>
2016-12-20 22:01:22 -0700

    added function which uses the stored array data and create table dynamically using DOM functionality

--- week2/victims-and-volunteers/main.js
+++ week2/victims-and-volunteers/main.js
@@ -16,24 +16,79 @@
 //   street.push(street);
 // }
 
-var nameArr = [];
-var phoneNumberArr = [];
-var street = [];
-
+var vArr = document.getElementById("Role");
 var nArr = document.getElementById("Name");
 var pArr = document.getElementById("PhoneNumber");
 var sArr = document.getElementById("StreetAddress");
 
+var victimArr = [];
+var volunteerArr = [];
+
+var obj = {
+    Role: '',
+    Name: '',
+    PhoneNumber: '',
+    StreetAddress: ''
+};
+
+
 function clearAndShow(){
+  vArr.value = "";
   nArr.value = "";
   pArr.value = "";
   sArr.value = "";
 }
 
 function insert(){
-   nameArr.push(nArr.value);
-   phoneNumberArr.push(pArr.value);
-   street.push(sArr.value);
 
+  obj['Role'] = vArr.value;
+  obj['Name'] = nArr.value;
+  obj['PhoneNumber'] = pArr.value;
+  obj['StreetAddress'] = sArr.value;
+
+  if(vArr.value === "victim"){
+      victimArr.push(obj);
+  }
+  else if(vArr.value === "volunteer"){
+      volunteerArr.push(obj);
+  }
    clearAndShow();
+   //  nameArr.push(nArr.value);
+   //  phoneNumberArr.push(pArr.value);
+   //  street.push(sArr.value);
+}
+
+function show(){
+    var table = document.createElement('table');
+    var row = document.createElement('tr');
+
+    for(var prop in obj){
+      var cell = document.createElement('th');
+      cell.textContent = prop;
+      row.appendChild(cell);
+    }
+    table.appendChild(row);
+
+    for(var i=0; i<victimArr.length; i++){
+      var row = document.createElement('tr');
+        for(var prop in victimArr[i]){
+          var cell = document.createElement('td');
+          cell.textContent = victimArr[i][prop];
+          row.appendChild(cell);
+        }
+      table.appendChild(row);
+    }
+
+    for(var i=0; i<volunteerArr.length; i++){
+      var row = document.createElement('tr');
+        for(var prop in volunteerArr[i]){
+          var cell = document.createElement('td');
+          cell.textContent = volunteerArr[i][prop];
+          row.appendChild(cell);
+        }
+      table.appendChild(row);
+    }
+
+    
+    console.log(table);
 }
